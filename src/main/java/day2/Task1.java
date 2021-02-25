df8061bb01a646b288f302c3ba5488c7eb4c91fd
peteroskovich <peter.sil@outlook.it>
2021-02-25 20:37:29 +0100

    Add Day2 Solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -7,13 +7,13 @@ public class Task1 {
         Scanner s = new Scanner(System.in);
         System.out.println("Введите количество этажей ");
         int a = s.nextInt();
-        if (a > 1 && a < 4) {
+        if (a >= 1 && a <= 4) {
             System.out.println("Малоэтажный дом");
-        } else if (a > 5 && a < 8) {
+        } else if (a >= 5 && a <= 8) {
             System.out.println("Среднеэтажный дом");
-        } else if (a > 9) {
+        } else if (a >= 9) {
             System.out.println("Многоэтажный дом");
-        } else if (a < 1) {
+        } else {
             System.out.println("Ошибка ввода");
         }
     }
