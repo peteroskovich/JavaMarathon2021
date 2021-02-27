19274491e5203d2a0cd5dbfa355c4ddd66052042
peteroskovich <peter.sil@outlook.it>
2021-02-27 23:24:39 +0100

    Add Day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            System.out.println(a / b);
+
+            if (b == 0)
+                break;
+
+        }
     }
 }
