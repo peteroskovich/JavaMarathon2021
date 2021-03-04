d2068874555f5395383426ae2f2bb31279e1177b
peteroskovich <peter.sil@outlook.it>
2021-03-04 22:10:29 +0100

    Add Day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -7,12 +7,14 @@ public class Task2 {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+
             double a = scanner.nextDouble();
             double b = scanner.nextDouble();
-            System.out.println(a / b);
-
             if (b == 0)
                 break;
+            System.out.println(a / b);
+
+
 
         }
     }
