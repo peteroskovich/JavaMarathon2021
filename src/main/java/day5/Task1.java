e646e9aa34d93cb9332c0223b4c201eb85c17393
peteroskovich <peter.sil@outlook.it>
2021-03-07 00:48:23 +0100

    Add Day5 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,42 @@
 package day5;
 
+class Car {
+     private int Age;
+    private String color;
+    private String model;
+
+    public void setColor(String colorName) {
+        color = colorName;
+             }
+    public void setModel (String modelName ) {
+        model = modelName;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setAge(int userAge) {
+        Age = userAge;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+}
+
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setAge(1997);
+        car1.setColor("Белый");
+        car1.setModel("БМВ");
+        System.out.println("Год выпуска " + car1.getAge());
+        System.out.println("Цвет модели " + car1.getColor());
+        System.out.println("Модель машины " + car1.getModel());
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,33 @@
 package day5;
+class Motorbike{
+    private int age;
+    private String color;
+    private String model;
 
+    public Motorbike(String color, String model, int age){
+        this.color=color;
+
+        this.model=model;
+        this.age=age;
+            }
+public String getColor(){
+        return color;
+    }
+    public String getModel(){
+        return model;
+    }
+    public int getAge(){
+        return age;
+    }
+
+}
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike mot= new Motorbike(" Черный " , " Ауди " ,10);
+        System.out.println(mot.getAge() + mot.getColor() + mot.getModel());
+
+
 
     }
 }
